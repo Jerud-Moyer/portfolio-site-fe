92c940e82e919acf281acfef002dbe1bfaaa84f5
Jerud-Moyer <jerudmoyer@gmail.com>
2021-05-21 22:14:20 -0700

    more navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -4,7 +4,8 @@ import styles from './NavBar.css';
 const NavBar = () => {
   return (
     <div className={styles.navBar} >
-      <a href={'#projects'} >projects</a>
+      <a href={'#home'}>home</a>
+      <a href={'#projects'}>projects</a>
       <a href={'#freelance'}>freelance</a>
       <a href={'#tech'}>tech stack</a>
       <a href={'#about'}>about me</a>
