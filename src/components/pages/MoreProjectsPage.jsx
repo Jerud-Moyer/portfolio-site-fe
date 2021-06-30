97b64eff8bf2a2ec2f426110ed623207f9a57136
Jerud-Moyer <jerudmoyer@gmail.com>
2021-06-30 16:13:20 -0700

    add mobile projects sub-section

--- src/components/pages/MoreProjectsPage.jsx
+++ src/components/pages/MoreProjectsPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { projects, moreProjects } from '../../data/projects';
+import { projects, moreProjects, mobileProjects } from '../../data/projects';
 import ProjectList from '../projects/ProjectList';
 
 const MoreProjectsPage = () => {
   return (
     <div>
-      <ProjectList projects={projects} />
       <ProjectList projects={moreProjects} />
+      <ProjectList projects={mobileProjects} />
+      <ProjectList projects={projects} />
     </div>
   );
 };
--- src/components/pages/ProjectsPage.jsx
+++ src/components/pages/ProjectsPage.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ProjectList from '../projects/ProjectList';
-import { projects } from '../../data/projects';
+import ScrollArrow from '../scroll-arrow/ScrollArrow';
+import { mobileProjects, projects } from '../../data/projects';
 import styles from './ProjectsPage.css';
 
 const ProjectsPage = () => {
   return (
-    <div className={styles.projectsPage} id={'projects'}>
+    <div 
+      className={styles.projectsPage} 
+      id={'projects'}
+    >
       <ProjectList projects={projects} forHomePage/>
+      <ProjectList projects={mobileProjects} mobile/>
+      <div className={styles.arrowBox}>
+        <ScrollArrow />
+      </div>
     </div>
   );
 };
