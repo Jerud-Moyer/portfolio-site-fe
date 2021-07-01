b3442adef7d5919bf4fdd729abc4ae76d6b554fe
Jerud-Moyer <jerudmoyer@gmail.com>
2021-07-01 16:58:57 -0700

    redesign moreProjects page and link to it

--- src/components/pages/MoreProjectsPage.jsx
+++ src/components/pages/MoreProjectsPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { projects, moreProjects, mobileProjects } from '../../data/projects';
 import ProjectList from '../projects/ProjectList';
+import styles from './MoreProjects.css';
 
 const MoreProjectsPage = () => {
+
   return (
-    <div>
+    <div className={styles.bigBox} >
+      <Link to={'/#projects'} className={styles.link} >
+        <div className={styles.backLink}>
+          {'<< back to main'}
+        </div>
+      </Link>
       <ProjectList projects={moreProjects} />
       <ProjectList projects={mobileProjects} />
       <ProjectList projects={projects} />
