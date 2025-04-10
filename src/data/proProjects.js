eafb74e4cf3b1f8fcb9e795f007b74a4ef04e7ca
Jerud Moyer <jerudmoyer@Jeruds-Most-Excellent-Machine.local>
2025-04-09 20:53:06 -0700

    add in Tony's site

--- src/data/proProjects.js
+++ src/data/proProjects.js
@@ -8,12 +8,19 @@ export const proProjects = [
     description: 'A full-stack web app for a multi-faceted Portland business. Amphead services musical equipment and Lanham builds custom Guitar Amplifiers. One website that acts as two. A NodeJS/Express backend API with PostgreSQL database, serves the data. A React/Redux front end brings it all to the consumer. Protected routes with full authorization allow the site owner to manage content through an administrator UI.',
   },
   {
-    name: 'michaelalbano.art',
-    imgUrl: '/assets/albano-small.png',
-    siteLink: 'https://www.michaelalbano.art/',
-    gitLink: 'https://github.com/Michael-Albano-Artist',
-    description: 'An artist\'s gallery web app with a React/TypeScript/Redux front-end and a NodeJS back-end that utilizes the Cloudinary API. The site owner can upload their own high quality images and manage all content with an admin UI through a protected route. With a mobile-first design, this app looks great on all devices.'
+    name: 'tonyventrella.com',
+    imgUrl: '/assets/tv-website.png',
+    siteLink: 'https://www.tonyventrella.com/',
+    gitLink: 'https://github.com/tony-ventrella-web',
+    description: 'A website and blog for a beloved Pacific Northwest sports personality. Built with NextJS and complete with a full fledged text editor and image uploader for blogging.'
   },
+  // {
+  //   name: 'michaelalbano.art',
+  //   imgUrl: '/assets/albano-small.png',
+  //   siteLink: 'https://www.michaelalbano.art/',
+  //   gitLink: 'https://github.com/Michael-Albano-Artist',
+  //   description: 'An artist\'s gallery web app with a React/TypeScript/Redux front-end and a NodeJS back-end that utilizes the Cloudinary API. The site owner can upload their own high quality images and manage all content with an admin UI through a protected route. With a mobile-first design, this app looks great on all devices.'
+  // },
   {
     name: 'eskart.net',
     imgUrl: '/assets/eskart.png',
